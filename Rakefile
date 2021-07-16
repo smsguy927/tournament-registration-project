db6a50a1ebbd0c0125ced7adfe7266779b07afa1
smsguy927 <smsguy927@gmail.com>
2021-07-16 13:58:59 -0500

    Minor fixes

--- Rakefile
+++ Rakefile
@@ -6,7 +6,13 @@ require "sinatra/activerecord/rake"
 desc "start console"
 task :console do
 
-  Pry.start
+  t14 = Tournament.find(14)
+  t14.close_registration
+  while t14.remaining_players > 0
+    t14.knockout_player(t14.active_player_ids.sample)
+  end
+
+  #Pry.start
 end
 
 
--- app/models/prize.rb
+++ app/models/prize.rb
@@ -4,7 +4,11 @@ class Prize < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :payout_table
 
-  def place
-    PayoutLine.all.find{|line|line.id == payout_line_id}.place
+  def calc_place
+    puts PayoutLine.all.map{|line| line.id}
+
+    puts payout_line_id
+    current_line = PayoutLine.all.find{|line|line.id == self.payout_line_id}
+    current_line.place
   end
 end
